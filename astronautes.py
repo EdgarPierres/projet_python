ec41ae3b139edd2bcf5fd890af6c68a89464fd60
Tyurin <anton.tyurin@etudiant.univ-rennes2.fr>
2026-03-27 11:16:48 +0100

    Ajout noms

--- astronautes.py
+++ astronautes.py
@@ -1,5 +1,5 @@
 import requests
-
+# Edgar Pierres / Anton Tyurin
 contenu = requests.get("http://api.open-notify.org/astros.json")
 for personne in contenu.json()["people"]:
     if personne["craft"] == "ISS":
